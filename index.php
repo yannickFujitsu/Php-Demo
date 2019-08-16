2876ad2125c714548918113ae777548d47566de0
Mark Hillis <markhillis@markhillis.net>
2019-08-16 19:16:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?php
-$myhostname = gethostname();
-echo "My Current Host: " . $myhostname ;
-=======
-echo "My Current Host: " . $myhostname ;
->>>>>>> 27929aa0f8588d75c32a8e9f6cb43532f49d8bac
+echo "my hostname is: " gethostname();
 ?>
