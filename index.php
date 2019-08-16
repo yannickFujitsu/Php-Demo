94401efca1960ff4ad70295181b99327a60480f5
Mark Hillis <markhillis@markhillis.net>
2019-08-16 18:58:09 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
 $myhostname = gethostname();
+<<<<<<< HEAD
 echo "My Current Host: " . $myhostname;
+=======
+echo "My Current Host: " . $myhostname ;
+>>>>>>> parent of 9a8e458... Update index.php
 ?>
+
